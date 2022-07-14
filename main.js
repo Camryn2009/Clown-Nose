445aff75a245ca1a3db0f22d748abe43d0635c8e
Camryn2009 <89714877+Camryn2009@users.noreply.github.com>
2022-07-13 20:45:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    clown_nose=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png')
+
 
 }
 
@@ -20,6 +25,11 @@ function modelLoaded(){
 
 function draw(){
 image(video, 0, 0, 300, 300);
+fill(255, 0, 0);
+stroke(255, 0, 0);
+//circle(noseX, noseY, 20);
+image(clown_nose, noseX, noseY, 30, 30);
+
 }
 
 function take_snapshot(){
@@ -29,7 +39,12 @@ function take_snapshot(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y-10;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+       
+        //console.log("nose x =" + results[0].pose.nose.x);
+        //console.log("nose y =" + results[0].pose.nose.y);
     }
 }
